2dd7b19b5aaaaedf5eab9027581891b8d845ed27
Aravindhsiva <adamaravindh@gmail.com>
2018-11-12 20:14:22 +0530

    Updates

---
+++ precache-manifest.c367e99a8eece22dcd34d394acfcf3c1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4de7cb4a2c6e0829601370826e208f7",
+    "url": "/furniture/static/media/chair.a4de7cb4.jpg"
+  },
+  {
+    "revision": "4013b8009c0639f0a452",
+    "url": "/furniture/static/css/main.1318438b.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/furniture/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9fb1ff91546f5b37713b",
+    "url": "/furniture/static/js/1.9fb1ff91.chunk.js"
+  },
+  {
+    "revision": "56a81cc66167c32ca45c",
+    "url": "/furniture/static/js/runtime~main.56a81cc6.js"
+  },
+  {
+    "revision": "448a762529cd1913b6ea46ba747883b0",
+    "url": "/furniture/static/media/logo.448a7625.jpg"
+  },
+  {
+    "revision": "1a42ea937bd4a88048366c929411e85a",
+    "url": "/furniture/static/media/one.1a42ea93.jpg"
+  },
+  {
+    "revision": "5a7c8762058836d6c275cf41eb8edc7a",
+    "url": "/furniture/static/media/two.5a7c8762.jpg"
+  },
+  {
+    "revision": "ced1bef8d1e331284faad34dc4f7c76f",
+    "url": "/furniture/static/media/three.ced1bef8.jpg"
+  },
+  {
+    "revision": "76be5a6c4c981a8fe4db85602be7ac87",
+    "url": "/furniture/static/media/lang.76be5a6c.png"
+  },
+  {
+    "revision": "4013b8009c0639f0a452",
+    "url": "/furniture/static/js/main.4013b800.chunk.js"
+  },
+  {
+    "revision": "3e3bfffa4acab157a6650dd14ad64795",
+    "url": "/furniture/static/media/lips.3e3bfffa.jpg"
+  },
+  {
+    "revision": "d437f6941550c37a66687065d67cbcfb",
+    "url": "/furniture/static/media/int.d437f694.jpg"
+  },
+  {
+    "revision": "6c0e08ed799151ec19513db4a0952a5b",
+    "url": "/furniture/static/media/large.6c0e08ed.jpg"
+  },
+  {
+    "revision": "22550f2f156008aa690a874a40324bac",
+    "url": "/furniture/static/media/wood.22550f2f.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/furniture/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/furniture/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/furniture/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/furniture/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "9fb1ff91546f5b37713b",
+    "url": "/furniture/static/css/1.9ae89236.chunk.css"
+  },
+  {
+    "revision": "409ba922dfbaf2fdc79323b96ff24629",
+    "url": "/furniture/index.html"
+  }
+];
